76cc7fcd4c8ddda8239b185e3f66b379aa303372
Vikash Kumar Sharma <vikashhrs@gmail.com>
2018-03-10 03:01:55 +0530

    added methods

--- app_logics/app_logic.js
+++ app_logics/app_logic.js
@@ -3,10 +3,12 @@ const request = require('request');
 const colors = require('colors');
 const auth = require('./../auth/auth');
 const moment = require('moment');
+const chalk = require('chalk');
 
 const rootUrl = "http://api.wordnik.com:80/v4/words.json/";
 
 module.exports.wordOfTheDay = function(){
+
 	let todaysDate = new moment().format("YYYY-DD-MM");
 	
 	request.get(
@@ -34,6 +36,154 @@ module.exports.wordOfTheDay = function(){
 		    			console.log("Source : "+term.partOfSpeech.bgMagenta);
 		    			console.log();
 		    		});
+		    	}
+		    }
+	)
+}
+
+module.exports.synonymsOfWord = function(word, limit = 5){
+	console.log(word,limit)
+	request.get(
+			{
+				url: "http://api.wordnik.com:80/v4/word.json/" + word + "/relatedWords", 
+				qs : { 
+					"useCanonical" : true, 
+					"relationshipTypes" : "synonym",
+					"limitPerRelationshipType" : limit,
+					"api_key" : auth.api_key
+				}
+			}, function(err, response, body) {
+
+				let jsonBody = JSON.parse(body);
+		    	
+		    	if(err){
+		    		throw err;
+		    	}else{
+		    		if(jsonBody.length != 0){
+
+		    			console.log("Synonyms of your word, "+chalk.bgYellow(word)+" are "+chalk.bgGreen(jsonBody[0].words))
+
+		    		}else{
+
+		    			console.log(chalk.bgRed("Sorry! No Synonyms at this time."))
+
+		    		}
+		    	}
+		    }
+	)
+}
+
+module.exports.antonymsOfWord = function(word, limit = 5){
+	console.log(word,limit)
+	request.get(
+			{
+				url: "http://api.wordnik.com:80/v4/word.json/" + word + "/relatedWords", 
+				qs : { 
+					"useCanonical" : true, 
+					"relationshipTypes" : "antonym",
+					"limitPerRelationshipType" : limit,
+					"api_key" : auth.api_key
+				}
+			}, function(err, response, body) {
+
+		    	let jsonBody = JSON.parse(body);
+
+		    	if(err){
+		    		throw err;
+		    	}else{
+		    		
+		    		if(jsonBody.length != 0){
+
+		    			console.log("Antonyms of your word, "+chalk.bgYellow(word)+" are "+chalk.bgGreen(jsonBody[0].words))
+
+		    		}else{
+
+		    			console.log(chalk.bgRed("Sorry! No antonyms at this time."))
+
+		    		}
+		    	}
+		    }
+	)
+}
+
+module.exports.examplesOfWord = function(word, skip = 0,limit = 5){
+
+	request.get(
+			{
+				url: "http://api.wordnik.com:80/v4/word.json/" + word + "/examples", 
+				qs : { 
+					"includeDuplicates" : false, 
+					"useCanonical" : false,
+					"skip" : skip,
+					"limit" : limit,
+					"api_key" : auth.api_key
+				}
+			}, function(err, response, body) {
+
+		    	let jsonBody = JSON.parse(body);
+
+		    	if(err){
+		    		throw err;
+		    	}else{
+		    		
+		    		if(jsonBody.examples.length != 0){
+
+		    			console.log("Examples of your word, "+chalk.green(word));
+
+		    			let examples = jsonBody.examples;
+
+		    			examples.forEach(function(example){
+		    				console.log("Title : " + chalk.cyan(example.title));
+		    				console.log("Text : " + chalk.cyan(example.text));
+		    				console.log();
+		    			});
+
+		    		}else{
+
+		    			console.log(chalk.bgRed("Sorry! No examples at this time."))
+
+		    		}
+		    	}
+		    }
+	)
+}
+
+module.exports.definitionsOfWord = function(word, limit = 5){
+
+	request.get(
+			{
+				url: "http://api.wordnik.com:80/v4/word.json/" + word + "/definitions", 
+				qs : { 
+					"limit" : limit, 
+					"includeRelated" : true,
+					"sourceDictionaries" : "all",
+					"useCanonical" : false,
+					"includeTags" : false,
+					"api_key" : auth.api_key
+				}
+			}, function(err, response, body) {
+
+		    	let jsonDefinitions = JSON.parse(body);
+
+		    	if(err){
+		    		throw err;
+		    	}else{
+		    		
+		    		if(jsonDefinitions.length != 0){
+
+		    			console.log("Definitions of your word, "+chalk.green(word));
+
+
+		    			jsonDefinitions.forEach(function(terms){
+		    				console.log(chalk.cyan(terms.text));
+		    				console.log();
+		    			});
+
+		    		}else{
+
+		    			console.log(chalk.bgRed("Sorry! No examples at this time."))
+
+		    		}
 		    	}
 		    }
 	)
--- wordhelp.js
+++ wordhelp.js
@@ -13,4 +13,47 @@ commander
   .action(App_Logic.wordOfTheDay());
 
 
+commander
+  .command('synonym_of <word>')
+  .description('Synonyms of word')
+  .action((word) => {
+  	console.log(word);
+  		App_Logic.synonymsOfWord(word);
+  });
+
+
+
+commander
+  .command('antonym_of <word>')
+  .description('Antonyms of word')
+  .action((word) => {
+  	console.log(word);
+  		App_Logic.antonymsOfWord(word);
+  });
+
+
+commander
+  .command('examples_of <word>')
+  .description('Examples of word')
+  .action((word) => {
+  		App_Logic.examplesOfWord(word);
+  });
+
+commander
+  .command('dictionary_of [words...]')
+  .description('Examples of word')
+  .action((words) => {
+  		if(words.length == 2){
+  			if(word[0] != "dict"){
+  				console.log("Invalid Command");
+  			}else{
+  				console.log("Valid Commands");
+  			}
+  		}else{
+
+  		}
+  });
+
+
+
 commander.parse(process.argv);
